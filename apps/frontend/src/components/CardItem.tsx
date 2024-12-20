eb465c6ec6758331f1310a2ff3e957fa885d2f09
marawanthedev <tmarawan11@gmail.com>
2024-12-20 15:34:14 +0800

    resolve prop drilling issue by using react context

--- apps/frontend/src/components/CardItem.tsx
+++ apps/frontend/src/components/CardItem.tsx
@@ -4,91 +4,92 @@ import { Box, Paper, TextField, Typography } from "@mui/material";
 import { Card } from "../types";
 import { CardStatus } from "packages/constants";
 import { Delete } from "@mui/icons-material";
+import { useBoardContext } from "../context";
 
 interface CardItemProps {
-  card: Card;
-  sourceColumn: CardStatus;
-  removeCard: (card: Card) => void;
-  updateCardContent: (id: number, content: string) => void;
+    card: Card;
+    sourceColumn: CardStatus;
+
 }
 
 export const CardItem: React.FC<CardItemProps> = ({
-  card,
-  sourceColumn,
-  removeCard,
-  updateCardContent,
+    card,
+    sourceColumn,
+
 }) => {
-  const [{ isDragging }, dragRef] = useDrag({
-    type: "CARD",
-    item: { card, sourceColumn },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
+    const [{ isDragging }, dragRef] = useDrag({
+        type: "CARD",
+        item: { card, sourceColumn },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
 
-  const [content, setContent] = useState(card.content);
-  const [isEditing, setIsEditing] = useState(false);
+    const { removeCard, updateCardContent } = useBoardContext();
 
-  const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateCardContent(card.id, event.target.value);
-    setContent(event.target.value);
-  };
+    const [content, setContent] = useState(card.content);
+    const [isEditing, setIsEditing] = useState(false);
 
-  const handleBlur = () => {
-    setIsEditing(false);
-    if (content !== card.content) {
-      updateCardContent(card.id, content);
-    }
-  };
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        updateCardContent(card.id, event.target.value);
+        setContent(event.target.value);
+    };
 
-  const handleClick = () => {
-    setIsEditing(true);
-  };
+    const handleBlur = () => {
+        setIsEditing(false);
+        if (content !== card.content) {
+            updateCardContent(card.id, content);
+        }
+    };
 
-  return (
-    <Paper
-      ref={dragRef}
-      sx={{
-        padding: "10px",
-        marginBottom: "8px",
-        backgroundColor: isDragging ? "#ddd" : "#fefefe",
-        opacity: isDragging ? 0.5 : 1,
-        boxShadow: 2,
-        cursor: "grab",
-        minHeight: "40px",
-      }}
-    >
-      {isEditing ? (
-        <TextField
-          autoFocus
-          variant="outlined"
-          fullWidth
-          value={content}
-          onChange={handleContentChange}
-          onBlur={handleBlur}
-          onClick={handleClick}
-        />
-      ) : (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
+    const handleClick = () => {
+        setIsEditing(true);
+    };
+
+    return (
+        <Paper
+            ref={dragRef}
+            sx={{
+                padding: "10px",
+                marginBottom: "8px",
+                backgroundColor: isDragging ? "#ddd" : "#fefefe",
+                opacity: isDragging ? 0.5 : 1,
+                boxShadow: 2,
+                cursor: "grab",
+                minHeight: "40px",
+            }}
         >
-          <Typography
-            variant="body1"
-            onClick={handleClick}
-            style={{ cursor: "pointer" }}
-          >
-            {content}
-          </Typography>
-          <Delete
-            onClick={() => removeCard(card)}
-            sx={{ color: "red", cursor: "pointer" }}
-          />
-        </Box>
-      )}
-    </Paper>
-  );
+            {isEditing ? (
+                <TextField
+                    autoFocus
+                    variant="outlined"
+                    fullWidth
+                    value={content}
+                    onChange={handleContentChange}
+                    onBlur={handleBlur}
+                    onClick={handleClick}
+                />
+            ) : (
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <Typography
+                        variant="body1"
+                        onClick={handleClick}
+                        style={{ cursor: "pointer" }}
+                    >
+                        {content}
+                    </Typography>
+                    <Delete
+                        onClick={() => removeCard(card)}
+                        sx={{ color: "red", cursor: "pointer" }}
+                    />
+                </Box>
+            )}
+        </Paper>
+    );
 };
--- apps/frontend/src/components/Column.tsx
+++ apps/frontend/src/components/Column.tsx
@@ -4,75 +4,66 @@ import { CardItem } from "./CardItem";
 import { Box, Button, Typography } from "@mui/material";
 import { CardStatus } from "packages/constants";
 import { Card } from "../types";
+import { useBoardContext } from "../context";
 
 interface ColumnProps {
-  title: CardStatus;
-  cards: Card[];
-  moveCard: (
-    sourceColumn: CardStatus,
-    targetColumn: CardStatus,
-    card: Card,
-  ) => void;
-  addCard: (column: CardStatus) => void;
-  removeCard: (card: Card) => void;
-  updateCardContent: (id: number, content: string) => void;
+    title: CardStatus;
+    cards: Card[];
 }
 
 type DropItem = {
-  sourceColumn: CardStatus;
-  card: Card;
+    sourceColumn: CardStatus;
+    card: Card;
 };
 
 export const Column: React.FC<ColumnProps> = ({
-  title,
-  cards,
-  moveCard,
-  addCard,
-  removeCard,
-  updateCardContent,
-}) => {
-  const [, dropRef] = useDrop({
-    accept: "CARD",
-    drop: (item: DropItem) => {
-      const droppedAt = title;
-      moveCard(item.sourceColumn, droppedAt, item.card);
-    },
-  });
+    title,
+    cards,
 
-  return (
-    <Box
-      ref={dropRef}
-      sx={{
-        flex: 1,
-        backgroundColor: "#f4f4f4",
-        padding: "16px",
-        borderRadius: "8px",
-        boxShadow: "0 1px 3px rgba(0,0,0,0.2)",
-      }}
-    >
-      <Typography
-        variant="h4"
-        sx={{ marginBottom: "10px", textTransform: "capitalize" }}
-      >
-        {title.toString().toLowerCase()}
-      </Typography>
-      {cards.map((card) => (
-        <CardItem
-          key={card.id}
-          card={card}
-          sourceColumn={title}
-          removeCard={removeCard}
-          updateCardContent={updateCardContent}
-        />
-      ))}
-      <Button
-        variant="contained"
-        sx={{ width: "100%" }}
-        onClick={() => addCard(title)}
-        style={{ marginTop: "16px" }}
-      >
-        Add New Card
-      </Button>
-    </Box>
-  );
+}) => {
+    const [, dropRef] = useDrop({
+        accept: "CARD",
+        drop: (item: DropItem) => {
+            const droppedAt = title;
+            moveCard(item.sourceColumn, droppedAt, item.card);
+        },
+    });
+
+    const { moveCard, addCard, removeCard, updateCardContent } = useBoardContext();
+
+
+    return (
+        <Box
+            ref={dropRef}
+            sx={{
+                flex: 1,
+                backgroundColor: "#f4f4f4",
+                padding: "16px",
+                borderRadius: "8px",
+                boxShadow: "0 1px 3px rgba(0,0,0,0.2)",
+            }}
+        >
+            <Typography
+                variant="h4"
+                sx={{ marginBottom: "10px", textTransform: "capitalize" }}
+            >
+                {title.toString().toLowerCase()}
+            </Typography>
+            {cards.map((card) => (
+                <CardItem
+                    key={card.id}
+                    card={card}
+                    sourceColumn={title}
+                />
+            ))}
+            <Button
+                variant="contained"
+                sx={{ width: "100%" }}
+                onClick={() => addCard(title)}
+                style={{ marginTop: "16px" }}
+            >
+                Add New Card
+            </Button>
+        </Box>
+    );
 };
